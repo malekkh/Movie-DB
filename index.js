0126bc6a5c6872d40060411facc2fac53d75610b
malekkh <malekkhoder98@gmail.com>
2022-12-25 11:07:56 +0200

    step 4

--- index.js
+++ index.js
@@ -25,3 +25,31 @@ app.get('/time', (req, res) => {
     message: `${hours}:${minutes}`
   });
 });
+
+// Let's complicate the API
+app.get('/hello/:id', (req, res) => {
+  const id = req.params.id;
+
+  res.status(200).json({
+    status: 200,
+    message: `Hello, ${id}`
+  });
+});
+
+app.get('/search', (req, res) => {
+  const search = req.query.s;
+
+  if (search) {
+    res.status(200).json({
+      status: 200,
+      message: 'ok',
+      data: search
+    });
+  } else {
+    res.status(500).json({
+      status: 500,
+      error: true,
+      message: 'you have to provide a search'
+    });
+  }
+});
